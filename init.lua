c6460126977376a3831226947ff7d93c46fc92e9
Infatoshi <elliot@arledge.net>
2024-06-22 06:30:33 -0600

    defaulted back to old monokai-pro + everforest theme

--- init.lua
+++ init.lua
@@ -782,28 +782,28 @@ require('lazy').setup({
     end,
   },
   --  {
-  --   'MeanderingProgrammer/markdown.nvim',
-  --   name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+  -- 'MeanderingProgrammer/markdown.nvim',
+  -- name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
   --   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   --    config = function()
   --      require('render-markdown').setup {}
   --    end,
   --  },
   { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    -- 'tanvirtin/monokai.nvim',
+    --    -- Change the name of the colorscheme plugin below, and then
+    --    -- change the command in the config to whatever the name of that colorscheme is
+    --    --
+    --    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+    --    -- 'tanvirtin/monokai.nvim',
     'loctvl842/monokai-pro.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      --      -- Load the colorscheme here.
+      --      -- Like many other themes, this one has different styles, and you could load
+      --      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'monokai-pro'
-
-      -- You can configure highlights by doing something like
+      --      -- vim.cmd.colorscheme 'monokai'
+      --      -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
     end,
   },
